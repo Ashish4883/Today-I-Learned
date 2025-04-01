7b23850c5a847191fb3f322cea8897563bea7500
Ashish4883 <aashishmishra0016@gmail.com>
2025-04-01 22:24:12 +0530

    updated supabase url

--- src/supabase.js
+++ src/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-const supabaseUrl = "https://mlfevegdoeeewdafeefx.supabase.co";
+const supabaseUrl = "https://ibapioofyozbuzqqalyf.supabase.co";
 const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1sZmV2ZWdkb2VlZXdkYWZlZWZ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDEyNjI5NjMsImV4cCI6MjAxNjgzODk2M30.6JPhipZTudXCBC9UnXxZ3GOQyjavIhnjTZnCOhChSQQ";
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImliYXBpb29meW96YnV6cXFhbHlmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1MjU3MjIsImV4cCI6MjA1OTEwMTcyMn0.jIUKbyxYBlR7tiacRobsR5wAzE8p0O8idBjUeZ4I0qQ";
 const supabase = createClient(supabaseUrl, supabaseKey);
 export default supabase;
